cf7d7e24635e4108c6ec10ece95d886e644952db
chunye <loseyear@gmail.com>
2020-02-14 17:07:27 +0800

    first

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,102 +1,8 @@
+function Random(min, max) {
+    return Math.floor(Math.random() * ((max-1) - (min+1))) + (min+1);
+}
 $(function () {
-  // 显示第一页
-  $('.js_step_1').show()
-  // 显示 menu
-  $('.js_menu').click(function() {
-    $('.js_menu_box').show()
-  })
-  // 隐藏 menu
-  $('.js_menu_box').click(function() {
-    $(this).hide()
-  })
-  // 显示 message
-  $('.js_message').click(function() {
-    $('.js_message_box').show()
-  })
-  // 隐藏 message
-  $('.js_message_box').click(function() {
-    $(this).hide()
-  })
-  // 创建 IO
-  const socket = io()
-  let room
-
-  //登录
-  $('.js_login').click(function() {
-    const value = $('.js_username').val()
-    if(!value) {
-      alert('请输入用户名')
-    } else {
-      $('.js_step_1').hide()
-      $('.js_step_2').show()
-      $('.js_name_w').text(value)
-      socket.emit('login', value)
-    }
-  })
-
-  // 显示所有用户数
-  socket.on('friends', data => {
-    console.log('用户数:', data)
-    $('.friends').text(data)
-  })
-  // 设置头像
-  socket.on('avatar', num => {
-    $('.js_avatar_w').attr('src', './image/' + num + '.jpg')
-  })
-
-  // 开始匹配
-  $('.start').click(function() {
-    socket.emit('waiting', null)
-    $('.js_step_2').hide()
-    $('.js_step_3').show()
-  })
-
-  // 分配房间后进入页面
-  socket.on('room', data => {
-    console.log(data)
-    const { room, name, topic, num } = data
-    console.log('房间号:', room)
-    $('.js_step_3').hide()
-    $('.js_step_4').show()
-    $('.js_avatar_m').attr('src', './image/' + num + '.jpg')
-    $('.js_name_m').text(name)
-    $('.js_topic').text(topic)
-    const roomm = 'room' + room
-    webrtc.joinRoom(roomm)
-    let second = 59
-    const clock = window.setInterval(function() {
-      second -= 1
-      $('.js_second').text(second)
-      if (second <= 0) {
-        clearInterval(clock)
-        close()
-      }
-    }, 1000)
-  })
-
-  // 提醒
-  $('.js_pop').click(function() {
-    alert('功能待完善')
-  })
-
-  // 退出
-  $('.js_quit').click(function() {
-    close()
-    socket.emit('close', room)
-  })
-
-  // 关闭
-  socket.on('close', data => {
-    if (data == room) close()
-  })
-
-  function close() {
-    $('.js_step_4').hide()
-    $('.js_step_2').show()
-    webrtc.leaveRoom()
-  }
-  // video start
-  webrtc = new SimpleWebRTC({
+  var webrtc = new SimpleWebRTC({
     localVideoEl: 'myVideo',
     remoteVideosEl: '',
     autoRequestMedia: true,
@@ -108,16 +14,100 @@ $(function () {
     },
     autoAdjustMic: true,
   })
+  const user = $.now()
+  // 创建 IO
+  const socket = io()
+  const room = 'room1'
+  /* 显示第一页
+  */
+  $('.page_1 .create').click(function() {
+    $('.page_1 .pop').show()
+  })
+  $('.page_1 .join').click(function() {
+    $('.page_1 .pop').show()
+  })
+  $('.page_1 .friend').click(function() {
+    $('.page_1').hide()
+    $('.page_2').show()
+    joinRoom()
+  })
 
+  $('.page_1 .add').click(function() {
+    $('.page_1').hide()
+    $('.page_2').show()
+    joinRoom()
+  })
+
+  function joinRoom() {
+    socket.emit('join', user)
+  }
+
+
+  /* 显示第二页
+  */
+  $('.page_2 .friend').click(function() {
+    $('.page_2 .friend_bg').show()
+    $('.page_2 .friend_list').show()
+  })
+  $('.page_2 .friend_bg').click(function() {
+    $('.page_2 .friend_bg').hide()
+    $('.page_2 .friend_list').hide()
+  })
+  $('.page_2 .friend_list').click(function() {
+    $('.page_2 .friend_bg').hide()
+    $('.page_2 .friend_list').hide()
+  })
+  $('.page_2 .tab li').click(function() {
+    const mark = $(this).attr('mark')
+    $('.page_2 .tool > div').hide()
+    $('.' + mark).show()
+    $('.page_2 .tab li').removeClass('active')
+    $(this).addClass('active')
+  })
+  $('.page_2 .biaoqing div img').click(function() {
+    const src = $(this).attr('src')
+    $('.page_2 .self img').attr('src', src)
+    $('.page_2 .self img').show()
+    clearImg()
+  })
+  function clearImg() {
+    clearTimeout(clearTime)
+    var clearTime = setTimeout(
+      function(){
+        $('.page_2 .self img').hide()
+      }, 3000
+    )
+  }
+  $('.page_2 .beijing div img').click(function() {
+    const src = $(this).attr('mark')
+    $('.page_2').css('background-image', "url(../image/" + src + ".png)")
+    $('.page_2').css('background-size', "100% 100%")
+  })
+  $('.page_2 .youxi div img').click(function() {
+    const mark = $(this).attr('mark')
+    if (mark === 's') {
+      const src = '../image/s_' + Random(0, 4) + '.png'
+      // 石头剪刀布
+      $('.page_2 .self img').attr('src', src)
+      $('.page_2 .self img').show()
+    } else {
+      const src = '../image/t_' + Random(0, 7) + '.png'
+      // 石头剪刀布
+      $('.page_2 .self img').attr('src', src)
+      $('.page_2 .self img').show()
+      // 骰子
+    }
+    clearImg()
+  })
+  socket.on('join', data => {
+    console.log(room)
+    console.log(data)
+    webrtc.joinRoom(room)
+  })
   webrtc.on('videoAdded', function (video, peer) {
-    $(video).appendTo('body')
-    $(video).attr('id', 'dest-' + peer.id)
+    console.log(video, peer)
+    // $(video).appendTo('body')
+    // $(video).attr('id', 'dest-' + peer.id)
     // $(video).hide()
   })
-
-  webrtc.on('videoRemoved', function (video, peer) {
-      var dest = $('video[id="dest-' + peer.id + '"]')
-      dest && dest.remove()
-  })
-  // video end
 })
--- bin/index.js
+++ bin/index.js
@@ -16,57 +16,12 @@ router.get('/', ctx => {
 
 app.use(router.routes())
 
-// 所有用户组
-let users = []
-// 待匹配用户组
-let pool = []
-// 房间号
-let room = 0
-
-const randomNum = maxNum => parseInt(Math.random() * (maxNum - 1),10)
-const topic = [
-  '北京的鬼天气咋又冷了',
-  '下雪了，炸鸡配啤酒？',
-  '年终奖。。。还是被裁员',
-  '“流浪地球”为啥被豆瓣黑惨了',
-  '二月不减肥，一年徒伤悲',
-  '萝莉音 Vs 御姐音',
-  '堵车 OR 一路畅通',
-  '初恋那件小事：）',
-  '“王者荣耀”啥段位，是大神不',
-  '今天吃鸡了吗：）',
-  '特斯拉modle3有兴趣不',
-  '汤圆 OR 元宵',
-  '燃烧我的卡路里!',
-  '一起说走就走旅行，敢吗',
-]
 io.on('connection', socket => {
   // 登录
-  socket.on('login', username => {
-    const num = (users.length + 1) % 50
-    users.push({
-      id: socket.id,
-      name: username,
-      num,
-    })
-    // 给自己以及其他人发在线用户数
-    socket.broadcast.emit('friends', users.length)
-    socket.emit('avatar', num)
-    socket.emit('friends', users.length)
-  })
-
-  // 断开连接
-  socket.on('disconnect', state => {
-    users = users.filter(v => v.id !== socket.id)
-    pool = pool.filter(v => v.id !== socket.id)
-  })
-
-  // 加入匹配
-  socket.on('waiting', () => {
-    // 进入匹配池
-    pool.push(socket.id)
-    // 排重
-    pool = Array.from(new Set(pool))
+  socket.on('join', id => {
+    console.log(id)
+    socket.broadcast.emit('join', id)
+    socket.emit('join', id)
   })
 
   // 退出
@@ -75,62 +30,6 @@ io.on('connection', socket => {
     socket.broadcast.emit('close', room)
   })
 })
-
-// 每十秒匹配一次
-setInterval(() => {
-  const group = arrSlice(pool)
-  for (let i = 0; i < group.length; i += 1) {
-    const one = group && group[i] && group[i][0]
-    const two = group && group[i] && group[i][1]
-    if (one && two) {
-      console.log(one, two)
-      room += 1
-      const t = topic[randomNum(topic.length)]
-      let numO = 0
-      let numT = 0
-      let nameO = ''
-      let nameT = ''
-      for(let i = 0; i < users.length; i += 1) {
-        if (users[i].id == one) {
-          numO = users[i].num
-          nameO = users[i].name
-        }
-        if (users[i].id == two) {
-          numT = users[i].num
-          nameT = users[i].name
-        }
-      }
-      io.sockets.connected[one].emit(
-        'room',
-        {
-          room,
-          name: nameT,
-          topic: t,
-          num: numT,
-        }
-      )
-      io.sockets.connected[two].emit(
-        'room',
-        {
-          room,
-          name: nameO,
-          topic: t,
-          num: numO,
-        }
-      )
-  // 选择话题
-      pool.splice(pool.indexOf(one), 1)
-      pool.splice(pool.indexOf(two), 1)
-    }
-  }
-}, 1000 * 10)
-
-// 数组两两分组
-const arrSlice = arr =>
-  arr
-    .sort(() => Math.random() > .5) // 打乱
-    .map((e, i) => i % 2 ? null : [arr[i], arr[i + 1]]) // 两两取出
-    .filter(Boolean)
 
 server.listen(3000, () => {
   console.log('listening on *:3000')
